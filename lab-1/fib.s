e04aa05f61ea5a2345f08f38df19fd109fba4246
GaryGu <gary640201a@gmail.com>
2014-09-25 11:25:14 +0800

    change name fib

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -4,51 +4,42 @@
 	.align 2
 	.thumb
 	.thumb_func
+	
+	.global fib
+	.type fib ,function
 
-	.global fibonacci
-	.type fibonacci, function
+@non - recursive
 
-fibonacci:
-	@ ADD/MODIFY CODE BELOW
-	@ PROLOG
-	push {r3, r4, r5, lr}
+fib:
+	@r0 = x (input)
+	push {r3 ,r4 ,r5 ,r6 ,lr}
+	
+	@r3 = previous
+	mov r3 ,#0
+	add r3 , r3 ,#4294967295
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-	subs r4 ,r0 ,#0
-	cmp r0 ,#0
-	blt .L3
+	@r4 = result
+	mov r4 ,#1
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-	cmp r4 ,#1
-	beq .L4
+	@r5 = itr index
+	mov r5 ,#0
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-	sub r0 ,r4 ,#1
-	bl fibonacci
+	@r6 = sum
+	mov r6 ,#0
+	
+itr:
+	cmp r5 ,r0
+	bgt finish
+	add r6 ,r4 ,r3
+	mov r3 ,r4
+	mov r4 ,r6
+	@ index ++
+	add r5 ,r5 ,#1
+	b itr
+finish:
+	mov r0 ,r4
+	pop {r3 ,r4 ,r5 ,r6 ,pc}
 
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-	mov r5 ,r0
-	sub r0 ,r4 ,#2
-	bl fibonacci
-
-	@ R0 = R5 + R0 (update flags)
-	adds r0 ,r5 ,r0
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-	.size fibonacci, .-fibonacci
+	.size fib, .-fib
 	.end
+
