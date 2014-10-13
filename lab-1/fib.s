316bd82aaaff35b467dd674252b19e3f3dbc2695
iGaryGu <gary640201a@gmail.com>
2014-10-13 21:12:51 +0800

    optimize the condition execution in Thumb-2

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -17,12 +17,16 @@ fibonacci:
 	@ if(R0 <= 0) goto .L3 (which returns 0)
 	subs r4 ,r0 ,#0
 	cmp r0 ,#0
-	blt .L3
+	itt ls
+	movls r0 ,#0
+	popls {r4, r5, pc}
 
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
 	cmp r4 ,#1
-	beq .L4
+	itt eq
+	moveq r0 ,#1
+	popeq {r4, r5, pc}
 
 	@ R0 = R4 - 1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
@@ -42,13 +46,6 @@ fibonacci:
 	pop {r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
